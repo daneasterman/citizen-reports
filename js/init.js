806d2f718024ba7818e2907965c0bfae0ebddb9b
Daniel Easterman <daniel.easterman@gmail.com>
2016-07-10 09:48:03 +0100

    Initial commit with firebase config

---
+++ js/init.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCHNB7UQmDx6vhuhhQ3uKe03yVvwRhTeME",
+    authDomain: "popping-heat-1609.firebaseapp.com",
+    databaseURL: "https://popping-heat-1609.firebaseio.com",
+    storageBucket: "popping-heat-1609.appspot.com",
+  };
+  firebase.initializeApp(config);
+    
