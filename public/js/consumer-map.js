0b2166f9d578cbb87a70dab7dedd6f39be0cd5a4
Daniel Easterman <daniel.easterman@gmail.com>
2016-09-11 18:14:57 +0100

    Refactored and separated out retrieve all and retrieve last report for notifications only

--- public/js/consumer-map.js
+++ public/js/consumer-map.js
@@ -2,21 +2,20 @@
  var mapOptions = { center: {lat: 0, lng: 0}, zoom: 3, disableDoubleClickZoom: true };
  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
- function retrieveLastReport() {
-  // Below here: limitToLast(1) - for one notification.
-  var reportsRef = firebase.database().ref('reports/').limitToLast(1);
+ function retrieveAllLatLng() {
+  
+  var reportsRef = firebase.database().ref('reports/');
   reportsRef.on('child_added', function(data) {
-
+    
     var lat = data.val().lat;
     var lng = data.val().lng;
     var msg = data.val().msg;
-    
-    generateMarker(lat, lng, msg);
-    generateNotification(msg);
+
+    generateMarkers(lat, lng, msg);
   });
  }
 
- function generateMarker(lat, lng, msg) {
+ function generateMarkers(lat, lng, msg) {
       var bounds = new google.maps.LatLngBounds();
       var latLng = new google.maps.LatLng(lat, lng);
       var marker = new google.maps.Marker({ position: latLng, map: map});
@@ -29,6 +28,15 @@
         iw.setContent('<p>'+msg+'</p>');
         iw.open(map, marker);
       });
+ }
+
+ function retrieveLastReport() {
+  
+  var reportsRef = firebase.database().ref('reports/').limitToLast(1);
+  reportsRef.on('child_added', function(data) {
+    var msg = data.val().msg;
+    generateNotification(msg);
+  });
  }
 
  function generateNotification(msg) {
@@ -65,5 +73,6 @@
  }
 
 $(function() {
-  retrieveMapData();
+  retrieveAllLatLng();
+  retrieveLastReport();
 });
